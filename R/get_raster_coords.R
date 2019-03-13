b96e58ff7bc1f847ceb233a73c21b6c0048a3271
Abram Fleishman <abram@conservationmetrics.com>
2019-03-12 22:50:36 -0700

    add documentation and change to new.dev

--- R/get_raster_coords.R
+++ R/get_raster_coords.R
@@ -1,6 +1,16 @@
+#' Get Analysis Selection from a list of Rasters
+#'
+#' Create a data frome with the analysis coordinants for each ultrasound image. this is an interactive plot that a user must click twice to select/create an alanysis selection comprised of coordinants along the x axis indicating the min and max that will be averaged/flattend by read ultra sound.  This is required before running read_ultrasound.
+#'
+#' @param raster_obj list object with rasters read in with raster_obj = purrr::map(paths_to_rasters,raster)
+#' @export
+#' @return a data frame with with analysis selection x,y coordinants
+#' @importFrom graphics plot
+
+
 get_raster_coords<-function(raster_obj){
   print(raster_obj@file@name)
-  quartz()
+  dev.new(noRStudioGD = T)
   plot(raster_obj)
   pt1<-click()
   pt2<-click()
