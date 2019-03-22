d5ffa3acd1b3ddf73e3198e20ac257e2a80fc0ad
Abram Fleishman <abram@conservationmetrics.com>
2019-03-21 23:12:05 -0700

    data doc

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Sample eseal analysis selections
+#'
+#' The resulting data coords from using get_raster_coords()
+#'
+#' @format A data frame with 55 rows and 5 variables:
+#' \describe{
+#'   \item{image_path}{price, in US dollars}
+#'   \item{xmin}{min x coord}
+#'   \item{xmax}{max x coord}
+#'   \item{ymin}{min y coord}
+#'   \item{ymax}{max y coord}
+#' }
+"eseal_coords"
