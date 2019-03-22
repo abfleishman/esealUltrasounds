1d16decf75d0e75d191c1b57eaba6f6d344f8bea
Abram Fleishman <abram@conservationmetrics.com>
2019-03-21 23:11:27 -0700

    bits and peices

--- prelim_analysis.R
+++ prelim_analysis.R
@@ -15,10 +15,10 @@ library(esealUltrasounds)
 ## tag depth
 
 # List Paths to PNGs
-paths=list.files("/git_repos/esealUltrasounds",pattern = ".png",full.names = T,recursive = T)[30]
+paths=list.files("/git_repos/esealUltrasounds",pattern = ".png",full.names = T,recursive = T)
 paths<-as.character(paths)
 # list all the image dirs
-image_dirs<-as.character(unique(dirname(paths)))[1]
+image_dirs<-as.character(unique(dirname(paths)))
 
 # read metadata
 eseal_meta<-map_dfr(image_dirs,read_eseal_meta)
@@ -39,9 +39,40 @@ rasters<-map(paths,raster)
 # plot(terrain(rat,opt = "roughness",unit = "degrees"))
 # plot(terrain(rat,opt = "flowdir",unit = "degrees"))
 
+
+
+
+# # Detect horizontal lines -------------------------------------------------
+#
+# # get image
+# img <- image_read(paths[2])
+# print(image_info(img))
+#
+# # define horizontal and vertical Sobel kernel
+# Shoriz <- matrix(c(2, 3, 2, 0, 0, 0, -2, -3, -2), nrow = 3)
+# Svert <- t(Shoriz)
+#
+# # get horizontal edges
+# imgV <- image_convolve(img, Svert,iterations = 2)
+#
+# hm<-imgV %>% image_canny(geometry = "1x10+80%+99%")
+#
+# a<-raster::as.raster(hm)
+# a[a!="#ffffffff"]<-0
+# a[a=="#ffffffff"]<-1
+#
+# b<- matrix(data = as.numeric(a),nrow = dim(a)[2],ncol = dim(a)[1])
+# plot(t(b) %>% raster)
+# b %>% rowSums() %>% plot
+# plot(a)
+
+
+# ne ----------------------------------------------------------------------
+
+
 # plot and get the analysis sections
 eseal_coords<-map_dfr(rasters,get_raster_coords)
-
+save(eseal_coords,file = "data/eseal_coords.rdata")
 # read rasters and flatten over analysis section (time dim)
 us_dat<-pmap_dfr(list(path=eseal_coords$image_path,xmin=eseal_coords$xmin,xmax=eseal_coords$xmax),read_ultrasound)
 
@@ -66,18 +97,18 @@ dat<-us_dat %>%
 
 dat %>%
   group_by(image_path) %>%
-  filter(drv1_smoothed3==max(drv1_smoothed3,na.rm = T)|)
+  filter(drv1_smoothed3==max(drv1_smoothed3,na.rm = T))
 
 # make four panel plots
 pdf('plots.pdf',width = 8,height = 8,onefile = T)
 for(i in 1:length(paths)){
-
   # get the coords for the analysis selection
   xs<-dat %>% ungroup %>% filter(image_path==paths[i]) %>%
     select(xmin,xmax,ymin,ymax,y_res) %>%
     distinct() %>%
     mutate(cms=(mean(c(ymin,ymax))/as.numeric(as.character(y_res)))-(973.5/as.numeric(as.character(y_res)))*100)
-
+  if(nrow(xs)==0) next
+  if(is.na(xs$cms)|nrow(xs)==0) next
   # get the raster ad a long data.frame
   hm<-as.data.frame(rasters[[i]],xy=T)
   names(hm)[3]<-"reflect"
@@ -87,18 +118,20 @@ for(i in 1:length(paths)){
                        ref_norm = scale(reflect,center = T,scale = T))
   xs$cms<-((mean(c(xs$ymin,xs$ymax))/as.numeric(as.character(xs$y_res)))*100)-(max(hm$y)/as.numeric(as.character(xs$y_res))*100)
   peaks<-area_under_peaks(dat %>% filter(image_path==paths[i]),
-                          dist_col = "cms",peaks_col = "drv1_smoothed2")
-  peaks %>%
+                          dist_col = "cms",peaks_col = "drv1_smoothed3")
+  chosen_ones<-peaks %>%
     mutate(cms_diff=abs(cms-xs$cms)) %>%
-    filter(AUC==max(AUC,na.rm=T)|drv1_smoothed2==max(drv1_smoothed2,na.rm=T)|cms_diff==min(cms_diff))
+    filter(AUC==max(AUC,na.rm=T)|drv1_smoothed3==max(drv1_smoothed3,na.rm=T)|cms_diff==min(cms_diff))
+
   plot(
     ggplot()+
       geom_raster(data=hm,aes(x,cms,fill=sqrt(reflect)))+
       geom_vline(xintercept = c(xs$xmin,xs$xmax ),col="red",size = 1,linetype=2)+
       geom_path(data=dat %>% filter(image_path==paths[i]),
                 aes(drv1_smoothed2,cms),col="grey80",show.legend = T)+
-      geom_point(data = peaks,aes(x=drv1_smoothed2,y=cms,col=type))+
+      geom_point(data = peaks,aes(x=drv1_smoothed3,y=cms,col=type))+
       geom_point(data = peaks,aes(x=AUC/10000,y=cms),col="red",pch=6)+
+      geom_point(data = chosen_ones,aes(x=drv1_smoothed3,y=cms),col="green")+
       scale_fill_gradient(low = "black",high = "white")+
       # scale_fill_viridis_c(option = "plasma" )+
       scale_y_continuous(breaks = seq(0,-9,-.5))+
